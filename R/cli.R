2841bf47f74ec81fdc01afdaca465ab94b82aea9
dgkf <18220321+dgkf@users.noreply.github.com>
2024-03-22 22:03:05 -0400

    initial cli poc

---
+++ R/cli.R
@@ -0,0 +1,84 @@
+cli_table_row <- function(status, ok, notes, warnings, errors, msg = "") {
+  fmt <- "│ {status} │ {ok} {notes} {warnings} {errors} │ {msg}"
+  cli::format_inline(fmt)
+}
+
+#' @export
+run <- function(packages) {
+  cli::ansi_with_hidden_cursor({
+    max_package_name_len <- max(nchar(names(packages)))
+
+    cat(
+      " ", strrep(" ", max_package_name_len), " ",
+      cli_table_row("S", "OK", " N", " W", " E"),
+      "\n",
+      sep = ""
+    )
+
+    # repeatedly update cli output for any active packages
+    alive <- 0
+    n_process <- 2
+    n_started <- 0
+
+    # print(environment())
+    cli::cli_progress_bar(
+      type = "custom",
+      format = "ETA {cli::pb_eta} ({cli::pb_current}/{cli::pb_total}) [{cli::pb_elapsed}]",
+      total = length(packages),
+    )
+
+    repeat {
+      alive <- alive + vipply(packages, function(i) inherits(i, "process") && i$is_alive())
+      n_alive <- sum(alive > 1)
+
+      if (n_alive < n_process) {
+        remaining <- !vlapply(packages, inherits, "process")
+        for (i in head(which(remaining), n_process - n_alive)) {
+          package <- names(packages[i])
+          packages[[i]] <- revcheck_process$new(
+            package,
+            args = "--no-manual",
+            build_args = "--no-manual"
+          )
+
+          cat(
+            " ",
+            strrep(" ", max_package_name_len - nchar(names(packages[i]))),
+            names(packages[i]),
+            " ",
+            packages[[i]]$cli_status_line(),
+            "\n",
+            sep = ""
+          )
+
+          n_started <- n_started + 1
+          alive[[i]] <- 2
+        }
+      }
+
+      statuses <- vcapply(packages, function(i) {
+        if (!inherits(i, "revcheck_process")) return("")
+        i$cli_status_line(width = cli::console_width() - max_package_name_len - 2)
+      })
+
+      for (i in which(alive > 0)) {
+        n <- n_started - i + 1
+        cat(
+          ansi_move_line_rel(n), ansi_line_erase(),
+          " ",
+          strrep(" ", max_package_name_len - nchar(names(packages[i]))),
+          names(packages[i]),
+          " ",
+          statuses[[i]],
+          ansi_move_line_rel(-n),
+          sep = ""
+        )
+      }
+
+      alive <- pmax(alive - 1, 0)
+      cli::cli_progress_update(set = n_started - sum(alive > 0))
+      if (!any(alive > 0)) break
+      Sys.sleep(0.1)
+    }
+  })
+}
---
+++ R/revcheck_process.R
@@ -0,0 +1,226 @@
+#' Regular Expression for Parsing R CMD check checks
+# nolint start
+RE_CHECK <- paste0(  
+  "(?<=^|\n)",       # starts on a new line (or start of string)
+  "\\* checking ",   # literal "* checking "
+  "(?<check>.*?)",   # capture any check content as "check"
+  " \\.\\.\\.",      # until literal "..."
+  "(?:",             # ignore additional check details:
+    "[\\s\\*]{2,}",  #   any content starting with two or more of spaces or "*"
+    ".*?(?:\n|$)",   #   until a newline (or end of string)
+  ")*",              #   repeating until
+  "(?<status>.*?)",  # capturing a status as "status"
+  "(?=\n|$)"         # terminated by a new line (or end of string)
+)                    
+# nolint end
+
+#' @importFrom R6 R6Class
+#' @importFrom rcmdcheck rcmdcheck_process
+revcheck_process <- R6::R6Class(
+  "revcheck_process",
+  inherit = rcmdcheck::rcmdcheck_process,
+  active = list(
+    checks = function() {
+      self$poll_output()
+      private$parsed_checks
+    },
+    last_check = function() {
+      self$poll_output()
+      private$parsed_last_check
+    }
+  ),
+  public = list(
+    initialize = function(...) {
+      args <- as.list(match.call(rcmdcheck::rcmdcheck, expand.dots = TRUE)[-1])
+      args <- lapply(args, eval, envir = parent.frame())
+
+      private$args <- args
+      private$throttle <- throttle()
+      private$spinners <- list(
+        check = silent_spinner("circleHalves"),
+        starting = silent_spinner(list(frames = c("⠴", "⠦", "⠖", "⠲")))
+      )
+
+      super$initialize(...)
+    },
+
+    get_time_last_check_start = function() {
+      private$time_last_check_start
+    },
+
+    get_time_finish = function() {
+      private$time_finish
+    },
+
+    poll_output = function() {
+      if (private$throttle()) return()
+      if (!self$is_alive()) {
+        private$time_last_check_start <- NULL
+        private$time_finish <- Sys.time()
+      }
+
+      out <- paste0(private$parsed_partial_check_output, super$read_output())
+      captures <- checks_capture(out)
+      checks <- checks_simplify(captures)
+      last_check <- tail(checks, 1L)  # might be NULL
+
+      if (length(checks) > 0) {
+        private$time_last_check_start <- Sys.time()
+        private$parsed_last_check <- last_check
+        private$parsed_checks[names(checks)] <- checks
+      }
+
+      if (is.null(last_check)) {
+        # no checks were parsed
+        private$parsed_partial_check_output <- out
+      } else if (identical(last_check, "")) {
+        # the final check was incomplete
+        n <- nrow(captures)
+        private$parsed_partial_check_output  <- substring(out, captures[1, n])
+      } else {
+        # the final check was fully parsed
+        private$parsed_partial_check_output  <- ""
+      }
+    },
+
+    cli_status_line = function(width = cli::console_width()) {
+      self$poll_output()
+      checks <- private$parsed_checks
+      last_check <- private$parsed_last_check
+
+      e <- cli::col_yellow(sprintf("%2.f", sum(checks == "ERROR")))
+      w <- cli::col_magenta(sprintf("%2.f", sum(checks == "WARNING")))
+      n <- cli::col_blue(sprintf("%2.f", sum(checks == "NOTE")))
+      o <- cli::col_none(sprintf("%2.f", sum(checks %in% c("NONE", "OK"))))
+
+      # runtime of process
+      process_time <- (self$get_time_finish() %||% Sys.time()) - self$get_start_time()
+      process_time <- paste0(format_time(process_time), " ")
+
+      # runtime of current check (only displayed if >30s)
+      check_time <- Sys.time() - self$get_time_last_check_start()
+      if (length(check_time) == 0 || check_time < difftime(30, 0)) {
+        check_time <- ""
+      } else {
+        check_time <- cli::col_grey("(", format_time(check_time), ") ")
+      }
+
+      msg <- ""
+      status <- " "
+      if (!length(last_check)) {
+        # have not hit checks yet
+        msg <- "starting ..."
+        status <- private$spinners[["starting"]]$spin()
+        process_time <- ""
+      } else if (self$is_alive()) {
+        # processing checks
+        msg <- paste("checking", names(last_check), "...")
+        status <- private$spinners[["check"]]$spin()
+        process_time <- cli::col_cyan(process_time)
+      } else {
+        # done
+        status_enum <- c("NONE", "OK", "NOTE", "WARNING", "ERROR")
+        status_code <- match(checks, status_enum)
+        status <- switch(max(status_code, 0, na.rm = TRUE),
+          "1" = , "2" = cli::col_green("✓"),
+          "3" = cli::col_blue("!"),
+          "4" = cli::col_magenta("?"),
+          "5" = cli::col_yellow("⨯")
+        )
+        process_time <- cli::col_grey(process_time)
+      }
+
+      msg <- cli::format_inline("{process_time}{check_time}{msg}")
+      out <- cli_table_row(status, o, n, w, e, msg)
+      cli::ansi_substring(out, 1, width)
+    }
+  ),
+  private = list(
+    args = list(),
+    time_last_check_start = NULL,
+    time_finish = NULL,
+    parsed_checks = character(0L),
+    parsed_last_check = character(0L),
+    parsed_partial_check_output = character(0L),
+    throttle = NULL,
+    spinners = NULL
+  )
+)
+
+#' Generate A Rate Limiting Throttle Function
+#'
+#' @param interval An interval (in seconds) that is the minimum interval
+#'   before [throttle] will return `TRUE`.
+#' @return A throttling function with the provided interval. When called,
+#'   returns a logical value indicating whether the throttle interval has
+#'   passed (TRUE if the interval has not yet passed).
+#'
+#' @examples
+#' my_throttle <- throttle(0.5)
+#'
+#' Sys.sleep(0.1)
+#' my_throttle()
+#'
+#' Sys.sleep(0.5)
+#' my_throttle()
+#'
+throttle <- function(interval = 0.2) {
+  e <- environment()
+  e$since <- Sys.time()
+  function(since = e$since, interval = e$interval) {
+    if (Sys.time() - since < interval) {
+      TRUE
+    } else {
+      e$since <- Sys.time()
+      FALSE
+    }
+  }
+}
+
+#' Parse R CMD checks from a partial check output string
+#'
+#' @param x A string, compsoed of any subsection of R CMD check console output
+#' @return A matrix of matches and capture groups "check" and "status"
+#'   ("OK", "NONE", "NOTE", "WARNING" or "ERROR").
+#'
+#' @examples
+#' check_output <- "
+#' * checking check one ... OK
+#' * checking check two ... NOTE
+#' * checking tests ...
+#'   Running test_abc.R
+#'   Running test_xyz.R
+#'  NONE
+#' * checking check three ... WARNING
+#' * ch
+#' "
+#'
+#' checks_capture(check_output)
+#'
+checks_capture <- function(x) {
+  m <- gregexec(RE_CHECK, x, perl = TRUE)[[1]]
+
+  if (!is.matrix(m)) {
+    captures <- c("check", "status")
+    return(t(matrix(character(0L), nrow = 3, dimnames = list(c("", captures)))))
+  }
+
+  l <- attr(m, "match.length")
+  t(matrix(
+    substring(x, m, m + l - 1),
+    nrow = nrow(m),
+    ncol = ncol(m),
+    dimnames = dimnames(m)
+  ))
+}
+
+#' Simplify Captures into Vector
+#'
+#' @param x Matrix of regex captures as produced by [checks_capture].
+#' @return A vector of check status, with names indicating the check
+#'
+checks_simplify <- function(x) {
+  checks <- trimws(x[, "status"])
+  names(checks) <- trimws(x[, "check"])
+  checks
+}
